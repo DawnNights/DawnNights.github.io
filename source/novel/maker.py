9e6d4c18f88d14492761cc3f3e669ce525e03d5d
DawnNights <2224825532@qq.com>
2021-12-05 23:17:17 +0800

    Site updated: 2021-12-05 23:17:17

--- source/novel/maker.py
+++ source/novel/maker.py
@@ -11,7 +11,7 @@ html = '''<html>
 
 <body>
     <a name="top"></a>
-    <ul class="menu_top_list book_view_top">
+    <ul class="menu_top_list book_view_top" style="top: -10px;height: 50px;">
         <li><a href="javascript:history.back()">返回</a></li>
         <li>%s</li>
         <li></li>
