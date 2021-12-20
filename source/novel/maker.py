45d070a29fd02e86eb0d90f519aa46cf1c316fa4
DawnNights <2224825532@qq.com>
2021-12-20 22:20:18 +0800

    Site updated: 2021-12-20 22:20:18

--- source/novel/maker.py
+++ source/novel/maker.py
@@ -1,30 +1,10 @@
 import os
 
-html = '''<html>
+pre = '''---
+title: %s
+---
 
-<head>
-    <meta charset="utf-8">
-    <meta name="description">
-    <title>夜黎的小文库</title>
-    <link href="https://rs.sfacg.com/web/m/css/v2/book-1.0.14.css" rel="stylesheet" type="text/css">
-</head>
-
-<body>
-    <a name="top"></a>
-    <ul class="menu_top_list book_view_top" style="top: -10px;height: 50px;">
-        <li><a href="/source/novel">返回</a></li>
-        <li>%s</li>
-        <li></li>
-    </ul>
-    <div class="yuedu Content_Frame" style="margin-top:0.4rem;">
-        <div style="text-indent: 2em;">
-            %s
-            <br><br><br><br><br><br><br>
-        </div>
-    </div>
-</body>
-<div class="bottom_menu"></div>
-</html>'''
+'''
 
 for path in os.listdir(os.getcwd()):
     if not path.endswith(".txt"):
@@ -34,12 +14,12 @@ for path in os.listdir(os.getcwd()):
 
     title = file.readline()
     content = ["<p>"+line+"</p>" for line in file.read().splitlines() if line != ""]
-    content = "\n".join(content)
+    content = "\n\n".join(content)
     file.close()
 
     os.remove(path)
 
-    file = open(path[:-4]+".html", "w", encoding="utf-8")
-    file.write(html%(title, content))
+    file = open(path[:-4]+".md", "w", encoding="utf-8")
+    file.write(pre%(title) + content)
     file.close()
     
